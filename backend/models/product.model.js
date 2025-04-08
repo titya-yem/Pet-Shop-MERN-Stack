b889ea578ba53de83d63edfab3a6fa4ebddab4f5
Titya Yem <thitya.yem.photo@gmail.com>
2025-04-08 11:45:54 -0400

    First commit

---
+++ backend/models/product.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+const productSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
